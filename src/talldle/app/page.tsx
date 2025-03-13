e1140e95ede01579eabaebcca4d96497af90bb71
Riley Wong <rnwong@scu.edu>
2025-03-12 18:51:06 -0700

    Set up next.js

---
+++ src/talldle/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="flex justify-center">
+        <div className="title text-5xl sm:text-6xl lg:text-7xl ">
+          <span className="text-talldle-red">Tall</span><span className="text-border text-talldle-red">dle</span>
+        </div>
+      </div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit neque quo dolore necessitatibus amet excepturi molestiae.</p>
+    </div>
+  )
+}
