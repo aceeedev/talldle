c748b95e76e4fefaad5d81ffcb90e8df282be20c
AceeeDev <cyberandrew03@gmail.com>
2025-03-13 16:51:14 -0700

    Added check to find outdated notion entries
    Also moved notion API request to utils

---
+++ data collection/utils.py
@@ -0,0 +1,63 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+NOTION_API = os.getenv('NOTION_API')
+NOTION_DATABASE_ID = os.getenv('NOTION_DATABASE_ID')
+
+
+class Entry:
+    def __init__(self, name, height, imageUrl, categories, sources):
+        self.name: str = name
+        self.height: int = height
+        self.image_url: str = imageUrl
+        self.categories: str = categories
+        self.sources: str = sources
+
+    def __str__(self):
+        return ", ".join(map(str, self.toList()))
+
+    def toList(self) -> list[str | int]:
+        return [self.name, self.height, self.image_url, self.categories, self.sources]
+
+
+def get_notion_entries() -> list[Entry]:
+    data: list[Entry] = []
+
+    headers = {
+        'Authorization': f"Bearer {NOTION_API}",
+        'Content-Type': 'application/json',
+        'Notion-Version': '2022-06-28',
+    }
+
+    not_done = True
+    start_cursor = ''
+    while (not_done):
+        body = {}
+        if start_cursor != '':
+            body = {
+                'start_cursor': start_cursor
+            }
+
+        response = requests.post(f"https://api.notion.com/v1/databases/{NOTION_DATABASE_ID}/query", headers=headers, json=body)
+        if response.status_code != 404:
+            json = response.json()
+
+            for row in json['results']:
+                props = row['properties']
+
+                name = props['Name']['title'][0]['plain_text']
+                height = props['Height (cm)']['number']
+                imageUrl = props['Image URL']['url']
+                categories = f"[{','.join(list(map(lambda x: x['name'], props['Tags']['multi_select'])))}]"
+                sources = f"[{','.join(list(map(lambda x: x['name'], props['Source']['multi_select'])))}]"
+
+                data.append(Entry(name, height, imageUrl, categories, sources))
+
+            if json['has_more']:
+                start_cursor = json['next_cursor']
+            else:
+                not_done = False
+
+    return data
