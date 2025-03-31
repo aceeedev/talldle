76c8e58c9678b663c5450d68df52cd53fb9985c1
Riley Wong <rnwong@scu.edu>
2025-03-30 18:48:02 -0700

    Re-style how to play

--- src/talldle/app/components/howToPlay.tsx
+++ src/talldle/app/components/howToPlay.tsx
@@ -12,7 +12,7 @@ export function showHowToPlay() {
 
 export function HowToPlay() {
   return (
-    <div onClick={hideHowToPlay} id="how-to-play" className="invisible fixed top-0 flex flex-col justify-center w-screen h-screen bg-[#00000050] hover:cursor-pointer z-1">
+    <div onClick={hideHowToPlay} id="how-to-play" className="invisible fixed top-0 sm:flex w-screen h-screen bg-[#00000050] hover:cursor-pointer z-1">
       <div onClick={(e) => e.stopPropagation()} className="relative m-auto bg-black p-8 sm:p-12 text-gray-100 hover:cursor-default" >
         <div onClick={hideHowToPlay} className="absolute top-2 sm:top-4 right-2 sm:right-4 w-12 sm:w-14 rounded-full p-2 hover:cursor-pointer hover:bg-[var(--dark-accent)]">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none">
@@ -23,10 +23,11 @@ export function HowToPlay() {
         <h1 className="text-2xl sm:text-4xl font-bold underline">How to Play</h1>
         <h2 className="text-lg">Sort the famous people by height with {maxNumGuesses} attempts.</h2>
         <br />
+        <p>Color scheme:</p>
         <ul className="list-disc list-inside">
-          <li><span className="text-green-300 underline">Green:</span> correct placement.</li>
-          <li><span className="text-yellow-300 underline">Yellow:</span> correct order but incorrect placement.</li>
-          <li><span className="text-gray-300 underline">Gray:</span> incorrect order and incorrect placement.</li>
+          <li><span className="text-green-300 underline">Green</span> = correct placement.</li>
+          <li><span className="text-yellow-300 underline">Yellow</span> = correct order but incorrect placement.</li>
+          <li><span className="text-gray-300 underline">Gray</span> = incorrect order and incorrect placement.</li>
         </ul>
         <br />
         <p>Incorrect guesses will guide you to the correct answer.</p>
