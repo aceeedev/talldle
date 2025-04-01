120618ab930bd45f87b1dab89c36a1f99d0c17f1
Riley Wong <rnwong@scu.edu>
2025-04-01 16:00:42 -0700

    Make empty column have aspect-square

--- src/talldle/app/components/emptyColumn.tsx
+++ src/talldle/app/components/emptyColumn.tsx
@@ -2,7 +2,7 @@ export function EmptyColumn() {
   return (
       <div className="grid grid-rows-7 gap-0.5 sm:gap-1">
         {[...Array(7)].map((_, i) => (
-          <div key={i} className="border-4 sm:border-6 border-[var(--dark-accent)] opacity-25" />
+          <div key={i} className="border-4 sm:border-6 border-[var(--dark-accent)] opacity-25 aspect-square" />
         ))}
       </div>
       
