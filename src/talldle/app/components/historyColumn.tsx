c4b4e1f6cf37b9aef1b236503c9dfc830555b994
Riley Wong <rnwong@scu.edu>
2025-04-06 20:29:16 -0700

    Set images to aspect square

--- src/talldle/app/components/historyColumn.tsx
+++ src/talldle/app/components/historyColumn.tsx
@@ -8,13 +8,13 @@ export function HistoryColumn({ order }: { order: Array<Guess> }) {
       {order?.length > 0 ? (
         order.map((item) => ( item.color == GuessColor.Gray || item.color == GuessColor.Green ? (
           <div key={item.celebs[0].id} className={`${item.color == GuessColor.Green ? 'no-drag border-green-400' : 'border-neutral-600'} row-span-1 border-4 sm:border-6 overflow-clip hover:cursor-pointer`}>
-            <img src={item.celebs[0].image} alt={item.celebs[0].name} className="object-cover brightness-70" />
+            <img src={item.celebs[0].image} alt={item.celebs[0].name} className="object-cover aspect-square brightness-70" />
           </div>
         ) : (
           <RowDiv rows={item.celebs.length} key={item.id}>
             {item.celebs.map((celeb) => (
               <div key={celeb.id} className="border-4 sm:border-6 border-yellow-400">
-                <img src={celeb.image} alt={celeb.name} className="object-cover brightness-80" />
+                <img src={celeb.image} alt={celeb.name} className="object-cover brightness-80 aspect-square" />
               </div>
             ))}
           </RowDiv>
